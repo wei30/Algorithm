6d67d7bd6930c30bf5216bfe4223765c8b679477
wei30 <weiyili54321@gmail.com>
2018-10-23 23:57:19 -0700

    Add files via upload
    implemented the delete node

--- LinkedListEx.java
+++ LinkedListEx.java
@@ -1,5 +1,7 @@
 package com.linkedlist;
 
+import com.linkedlist.LinkedListExample.Node;
+
 public class LinkedListEx {
 	Node head;
   static class Node{
@@ -14,7 +16,7 @@ public class LinkedListEx {
 	  //insert node at the last
 	  public void insert(int data){
 		  Node currentNode = head;
-		  if(head == null){
+		  if(head == null){ //if there is no node 
 			  head = new Node(data);//create a new node
 			  return;
 		  }
@@ -25,12 +27,60 @@ public class LinkedListEx {
 		  //create new node if is reach at the end
 		  Node newNode = new Node(data);
 		  currentNode.next = newNode;
-	  
   }
+	  public void delete2(int data){
+		   Node currentNode = head;
+		   Node previousNode = null;
+		   if(currentNode == null){
+			   System.out.println("Empty");
+		   }
+		   
+		   //loop through the list and if no value found 
+		   while(currentNode != null && currentNode.data != data){
+			   previousNode = currentNode; //currentNode equal to the previous node
+			   currentNode = currentNode.next; //move next node
+		   }
+		   
+		   if(currentNode != null && currentNode.data == data){
+			   //given value is found, then delete it
+			   head = currentNode.next; //now the currentNode.next(next value) is head  
+		   }
+		   previousNode.next = currentNode.next;
+	   }
+	  //delete node if it finds matching node
+	  public void delete(int data){
+		  Node currentNode = head; 
+		  Node previousNode = null; //previous(->)
+		  
+		  if(currentNode == null){
+			   System.out.println("Empty");
+		   }
+		  
+		  while(currentNode != null && currentNode.data != data){
+//			  currentNode.next = head;
+//			  previousNode.next = currentNode.next; //now make the preivousNode skip one node
+			  previousNode = currentNode; //now previousNode is the currentNode
+			  currentNode = currentNode.next; //move next
+//			  if(currentNode.data == data){ //if currentNode found
+//				  head = currentNode.next;
+//				  previousNode.next = currentNode.next;
+//			  }
+		  }
+		  if(currentNode != null && currentNode.data == data){ //if currentNode found
+			  head = currentNode.next;
+			  
+		  }
+		  previousNode.next = currentNode.next;
+	  }
+	  
   public static void main(String[] args) {
 	LinkedListEx list = new LinkedListEx();
      list.insert(1);
      list.insert(2);
+     list.insert(3);
+     list.insert(4);
+     list.insert(5);
+     list.delete2(2);
      list.printLinkedList();
   }
 public void printLinkedList() {
