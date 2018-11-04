f23a5e9308a3ff3bc56ac09766e2351861048e85
wei30 <weiyili54321@gmail.com>
2018-11-04 12:07:33 -0800

    Update FindPair.java

--- Sorting/FindPair.java
+++ Sorting/FindPair.java
@@ -1,4 +1,11 @@
-public class MissingNum{
-	while(low <= high){
+public class FindPair{
+	//https://java2blog.com/find-pair-whose-sum-is-closest-to-zero-in-array/
+	//sort the array 
+	//give a left pointer and a right pointer 
+	//check the sum<0 move the left pointer forward
+	//else check the sum > 0 move the right pointer backward 
+	int minSum = Integer.Max_Value;
+	while(left < right){
 	}
+	
 }
