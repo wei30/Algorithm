d26971166b5fce9a76e7b420a7965888d8303e07
wei30 <weiyili54321@gmail.com>
2018-11-04 14:23:15 -0800

    Update FindSecondLargest.java

--- Sorting/FindSecondLargest.java
+++ Sorting/FindSecondLargest.java
@@ -1,5 +1,12 @@
 public class FindSecondLargest{
 	int[] arr={7,5,6,1,4,2};
+	int secondlargest=0;
+	int largest=0;
 	for(int i=0; i<arr.length; i++){
+		//loop around then find the second largest, first find the largest first and from there you find the second largest
+		if(arr[i] > largest){
+		
+		}else if(arr[i] > secondlargest){
+		}
 	}
 }
