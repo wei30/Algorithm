5c144172fb21cad0c864b698357c94fb6e098a86
wei30 <weiyili54321@gmail.com>
2018-11-03 21:58:25 -0700

    Update MissingNumber.java

--- Sorting/MissingNumber.java
+++ Sorting/MissingNumber.java
@@ -1,4 +1,13 @@
 public class MissingNum{
 	while(low <= high){
+		mid = low + ((high-low)/2);
+		if(arr[mid] == num){
+			return mid;
+		}
+		if(arr[mid <=a[high]){
+			if(num > arr[mid] && num <=arr[high]){
+				low = mid + 1;
+			}
+		}
 	}
 }
