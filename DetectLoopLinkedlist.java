bfabd4444a66d77cadf8a75a4661162d85adeaf9
wei30 <weiyili54321@gmail.com>
2018-11-13 15:09:43 -0800

    Update DetectLoopLinkedlist.java

--- DetectLoopLinkedlist.java
+++ DetectLoopLinkedlist.java
@@ -1,5 +1,15 @@
 public boolean detectLoopLinkedList(){ //return true if there loop in linkedlist else no loop
 	//to determine if there is loop first create a faster node and slower node 
 	//if they met then it is a loop else is not a loop
-	
+	//both start head
+	Node fastptr = head;
+	Node slowptr = head;
+	while(fastptr != null && fastptr.next != null){
+		if(fastptr == slowptr){
+			//they are equal 
+			//found a loop
+			return true;
+		}
+		
+	}
 }
