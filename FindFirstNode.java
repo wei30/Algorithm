84096e1b8d3bd909b7df6683d88d899cd1411896
wei30 <weiyili54321@gmail.com>
2018-11-13 15:45:41 -0800

    Update FindFirstNode.java

--- FindFirstNode.java
+++ FindFirstNode.java
@@ -1,3 +1,18 @@
-public Node findNode(){
-	
+//To find the firstNode of loop of linkedlist
+// first determine if there is a loop first
+//after detect if there is a loop, move both nodes by one if both
+//node match then thats the starting node
+public Node findFirstNode(){
+	Node slowptr = head;
+	Node fastptr = head;
+	boolean loop = false;
+	while(fastptr != null && fastptr.next != null){
+		if(fastptr == slowptr){
+			loop = true;
+			break;
+		}
+		fastptr = fastptr.next.next;
+		slowptr = slowptr.next;
+	}
+
 }
