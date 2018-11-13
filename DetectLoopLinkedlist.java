5f378c2fdf9eef2f66811c3d02229fea82f63a97
wei30 <weiyili54321@gmail.com>
2018-11-13 15:07:29 -0800

    Update DetectLoopLinkedlist.java

--- DetectLoopLinkedlist.java
+++ DetectLoopLinkedlist.java
@@ -1,4 +1,5 @@
-public class MissingNum{
-	while(low <= high){
-	}
+public boolean detectLoopLinkedList(){ //return true if there loop in linkedlist else no loop
+	//to determine if there is loop first create a faster node and slower node 
+	//if they met then it is a loop else is not a loop
+	
 }
